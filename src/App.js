8c3491e699e98630ba4a89f0324957bf610dfe60
Vijeta Singh Rajput <vijetasinghrajput@gmail.com>
2023-11-20 21:36:02 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
     <footer>
       This project is created by Vijeta Singh and it is{" "}
       <a href="https://github.com/vijetarajput/Weather_Project_React" target="_blank" rel="noreferrer">Open-sourced on GitHub</a>
     </footer>
+    </div>
     </div>
   );
 }
