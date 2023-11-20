b4547d0720524359c1eff8d26c309cf5ec5d2e8d
Vijeta Singh Rajput <vijetasinghrajput@gmail.com>
2023-11-20 21:13:28 +0000

    Added GitHub

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     My Weather App
+    <footer>
+      This project is created by Vijeta Singh and it is{" "}
+      <a href="https://github.com/vijetarajput/Weather_Project_React" target="_blank" rel="noreferrer">Open-sourced on GitHub</a>
+    </footer>
     </div>
   );
 }
-
 export default App;
